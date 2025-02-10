8731b5ea3cf4ecbb963aa87a5c908334bb9cc153
MarcosCast <marcosmax.5@gmail.com>
2025-02-09 22:57:58 -0300

    feat(redis): adiciona configuração completa do Redis (módulo, serviço, testes e ajustes)

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { createClient, RedisClientType } from 'redis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+  private client: RedisClientType;
+
+  async onModuleInit() {
+    this.client = createClient({ url: 'redis://localhost:6379' });
+    await this.client.connect();
+  }
+
+  getClient(): RedisClientType {
+    return this.client;
+  }
+
+  async onModuleDestroy() {
+    await this.client.quit();
+  }
+}
