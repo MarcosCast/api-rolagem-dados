63e5a101c59e582ee5bd24026821b24c9fd14122
MarcosCast <marcosmax.5@gmail.com>
2025-02-13 21:12:12 -0300

    fix(app): corrige a resposta do endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
-    return 'API está rodndo!';
+
+    return 'API está rodando!';
+
   }
+  
 }
